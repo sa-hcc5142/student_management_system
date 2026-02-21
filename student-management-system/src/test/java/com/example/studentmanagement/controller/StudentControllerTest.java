d46a71de69023efe235e25020e09b24f323e075e
sa-hcc5142 <sumaiyaakter0248.gmail.com>
2026-02-22 00:19:52 +0600

    created student management system with authintication & authorization

---
+++ student-management-system/src/test/java/com/example/studentmanagement/controller/StudentControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.studentmanagement.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentControllerTest {
+
+    @Test
+    void getAllStudents() {
+    }
+
+    @Test
+    void getStudentById() {
+    }
+}
